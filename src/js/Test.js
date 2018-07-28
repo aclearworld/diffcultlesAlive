2cf4255bf81433c1441406046de3949e70720823
a <yamamichi1995@outlook.jp>
2018-07-28 09:24:29 +0900

    installed libs

---
+++ src/js/Test.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class Test extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      int: props.int,
+      str: props.str,
+      bar: props.bar,
+    };
+  }
+
+  render() {
+    const states = this.state;
+    const { int } = this.state;
+    console.log(states);
+    console.log(int);
+    return (
+      <div>
+        <h3>{states.int}</h3>
+        <h3>{states.str}</h3>
+        <h3>{states.bar.str}</h3>
+      </div>
+    );
+  }
+}
+
+Test.propTypes = {
+  int: PropTypes.number.isRequired,
+  str: PropTypes.string.isRequired,
+  bar: PropTypes.object.isRequired,
+};
+
+Test.defalutProps = {
+  int: 0,
+  str: '',
+  obj1: {},
+};
