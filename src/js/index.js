9c09daf17e36f4f41a20a9c47049562646f9fc19
a <yamamichi1995@outlook.jp>
2018-07-28 09:22:25 +0900

    move file

---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import style from './index.css';
+import Test from './Test';
+
+const dom = (
+  <div className={style.world}>
+    <h1>hello world</h1>
+    <Test int={45} str="str" bar={{ str: 'bar' }} />
+  </div>
+);
+
+ReactDOM.render(dom, document.querySelector('#content'));
