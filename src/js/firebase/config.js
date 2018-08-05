b7427601568de843922bb28dc859c24668d948f9
Unknown <yamamichi1995@outlook.jp>
2018-08-05 22:58:44 +0900

    install firebase

---
+++ src/js/firebase/config.js
@@ -0,0 +1,10 @@
+const config = {
+  apiKey: 'AIzaSyAA9Jnfkr4BkUWhhsRQ5xjzh_Xw09NwKyU',
+  authDomain: 'difficultiesalive.firebaseapp.com',
+  databaseURL: 'https://difficultiesalive.firebaseio.com',
+  projectId: 'difficultiesalive',
+  storageBucket: 'difficultiesalive.appspot.com',
+  messagingSenderId: '285179935939',
+};
+
+export default config;
--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import firebase from 'firebase/app';
 import App from './components/App';
+import config from './firebase/config';
+
+const firebaseApp = firebase.initializeApp(config);
 
 const dom = (
   <div>
